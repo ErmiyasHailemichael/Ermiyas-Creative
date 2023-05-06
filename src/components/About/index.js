95407c9cf0767682b4cdd09e3b3ef06802938f21
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-05-05 22:13:28 -0700

    about section is styled

---
+++ src/components/About/index.js
@@ -0,0 +1,31 @@
+import './index.scss'
+
+const About = () => {
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>About me</h1>
+        <p>
+          Greetings! I graduated from General Assembly's Software Engineering
+          Immersive boot camp, where I acquired valuable experience working on
+          real-world projects and sharpened my skills in collaborative
+          development, agile methodologies, and problem-solving.
+        </p>
+        <p>
+          My ability to thrive in team-based software development environments
+          makes me an ideal candidate for any dynamic and innovative team. As a
+          part-time teacher at Coding With Kids, I teach K-12 students how to
+          code in Scratch, Python, Javascript, HTML, and CSS.
+        </p>
+        <p>
+          I am also currently studying Computer Science at Seattle Central
+          College, as I am passionate about staying up-to-date with the latest
+          technologies and industry trends. I am always eager to learn and grow
+          as a developer by taking on new challenges and opportunities.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default About
