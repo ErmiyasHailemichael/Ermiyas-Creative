264418e740ffda454d425d1efd1086fd78e0f7fc
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2023-05-05 22:39:20 -0700

    skills added

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJsSquare, faReact, faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import './index.scss'
 
 const About = () => {
+  const skills = [
+    { name: 'HTML5', icon: faHtml5 },
+    { name: 'CSS3', icon: faCss3Alt },
+    { name: 'JavaScript', icon: faJsSquare },
+    { name: 'React', icon: faReact },
+    { name: 'Node.js', icon: faNodeJs },
+  ]
   return (
     <div className="container about-page">
       <div className="text-zone">
@@ -23,6 +32,17 @@ const About = () => {
           technologies and industry trends. I am always eager to learn and grow
           as a developer by taking on new challenges and opportunities.
         </p>
+        <div>
+          <h2>Skills</h2>
+          <ul>
+            {skills.map((skill, index) => (
+              <li key={index}>
+                <FontAwesomeIcon icon={skill.icon} />
+                <span>{skill.name}</span>
+                </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
